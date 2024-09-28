6d45e840277eeb6cbc4c441ec4271bab13ded9af
David Mraz <david@atheros.ai>
2024-09-28 20:55:17 +0200

    feat: detect widget mistake

--- widget-02/script.js
+++ widget-02/script.js
@@ -3,5 +3,5 @@ const dragAndDropItems = document.getElementById("stocks");
 new Sortable(dragAndDropItems, {
   animation: 350,
   chosenClass: "stock-chosen",
-  dragClass: "stock-drag",
+  dragClass: "stock-drg",
 });
